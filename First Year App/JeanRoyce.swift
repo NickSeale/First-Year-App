50a748b86c940330d9f16e4f7adaf2369f073b67
alexjjseppala <alexjjseppala@gmail.com>
2015-03-18 16:31:53 -0400

    Maps, food and some constraints

---
+++ First Year App/JeanRoyce.swift
@@ -0,0 +1,35 @@
+//
+//  JeanRoyce.swift
+//  First Year App
+//
+//  Created by Alex Seppala on 2015-03-18.
+//  Copyright (c) 2015 Nicholas Seale. All rights reserved.
+//
+
+import UIKit
+
+class JeanRoyce: UIViewController {
+ 
+    @IBOutlet var JeanRoyceWeb: UIWebView!
+    
+    var URLPath = "http://troywolters.com/app/menu/JeanRoyce"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        loadAddressURL()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func loadAddressURL(){
+        let requestURL = NSURL(string:URLPath)
+        let request = NSURLRequest(URL: requestURL!)
+        JeanRoyceWeb.loadRequest(request)
+    }
+    
+}

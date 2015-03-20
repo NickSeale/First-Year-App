71b3a55f247e54b6a66670b342f34ef2549f5836
alexjjseppala <alexjjseppala@gmail.com>
2015-03-20 16:14:51 -0400

    External Links Added
    External links….they’ve been added

---
+++ First Year App/QueensU.swift
@@ -0,0 +1,34 @@
+//
+//  QueensU.swift
+//  First Year App
+//
+//  Created by Alex Seppala on 2015-03-20.
+//  Copyright (c) 2015 Nicholas Seale. All rights reserved.
+//
+
+import UIKit
+
+class QueensU: UIViewController {
+    
+    @IBOutlet var QueensUWeb: UIWebView!
+    var URLPath = "http://queensu.ca/"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        loadAddressURL()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func loadAddressURL(){
+        let requestURL = NSURL(string:URLPath)
+        let request = NSURLRequest(URL: requestURL!)
+        QueensUWeb.loadRequest(request)
+    }
+    
+}

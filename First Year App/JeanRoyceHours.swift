8643deb2b9dd621368f8ea36304bd794dee020b4
NickSeale <nicholas.a.seale@gmail.com>
2015-04-02 13:36:52 -0400

    Update 02/04/15
     - Added Scroll wheel for jean royce hours
     - changed appearance of food buttons

---
+++ First Year App/JeanRoyceHours.swift
@@ -0,0 +1,36 @@
+//
+//  JeanRoyceHours.swift
+//  First Year App
+//
+//  Created by Kathryn Ohashi on 2015-04-02.
+//  Copyright (c) 2015 Nicholas Seale. All rights reserved.
+//
+
+import UIKit
+
+class JeanRoyceHours: UIViewController {
+
+    @IBOutlet weak var ScrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        ScrollView.contentSize.height = 500
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
